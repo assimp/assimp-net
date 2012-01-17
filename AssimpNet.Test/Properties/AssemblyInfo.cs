3eabd8cc25502c01aa4cbe0cc11d96b8b65e9b2a
nicholas.woodfield@gmail.com <nicholas.woodfield@gmail.com@f94b5969-1416-58a3-bd65-97f45d67da10>
2012-01-17 05:27:13 +0000

    Updated assembly info

--- AssimpNet.Test/Properties/AssemblyInfo.cs
+++ AssimpNet.Test/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AssimpNet.Test")]
+[assembly: AssemblyTitle("Assimp .Net Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("AssimpNet.Test")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCompany("Tesla Engine Group")]
+[assembly: AssemblyProduct("Assimp .Net")]
+[assembly: AssemblyCopyright("Copyright © Nicholas Woodfield 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
