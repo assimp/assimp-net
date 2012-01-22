c7e67e54ff523be7c928977d9ffc035b0ee36ae1
nicholas.woodfield@gmail.com <nicholas.woodfield@gmail.com@f94b5969-1416-58a3-bd65-97f45d67da10>
2012-01-22 02:18:03 +0000

    Added license header. Removed/sorted usings.

--- AssimpNet/Animation.cs
+++ AssimpNet/Animation.cs
@@ -21,7 +21,6 @@
 */
 
 using System;
-using System.Globalization;
 using System.Runtime.InteropServices;
 using Assimp.Unmanaged;
 
--- AssimpNet/NodeAnimationChannel.cs
+++ AssimpNet/NodeAnimationChannel.cs
@@ -21,8 +21,6 @@
 */
 
 using System;
-using System.Globalization;
-using System.Runtime.InteropServices;
 using Assimp.Unmanaged;
 
 namespace Assimp {
