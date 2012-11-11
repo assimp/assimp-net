fa376577d624f8a9252998a04983aa7ed0cd2fb2
nicholas.woodfield@gmail.com <nicholas.woodfield@gmail.com@f94b5969-1416-58a3-bd65-97f45d67da10>
2012-11-11 06:29:53 +0000

    Tagged 3.0 release

---
+++ tags/3.0/AssimpNet.Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assimp.Sample {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            SimpleOpenGLSample sample = new SimpleOpenGLSample();
+            sample.Run(30.0, 0.0);
+        }
+    }
+}
